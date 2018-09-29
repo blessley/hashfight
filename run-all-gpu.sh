c3f5c8040b8cce80f9e2090569b832df1cd130d9
Brent Lessley <blessley@lbl.gov>
2018-09-28 22:58:15 -0700

    Initial commit of v100 branch

--- run-all-gpu.sh
+++ run-all-gpu.sh
@@ -13,13 +13,13 @@ failure='0'
 failure_trials=10
 all_trials=10
 
-: <<'COMMENT'
+#: <<'COMMENT'
 #run through all the sizes for a factor-failure configuration:
 for l in $factors; do #load factors
   for f in $failure; do #failure rates
     filename_config_times="${timings_OUT_DIR}/${l}-${f}-${failure_trials}"
     touch $filename_config_times
-    for k in {50000000..500000000..50000000}; do #num key-val pairs
+    for k in {50000000..1450000000..50000000}; do #num key-val pairs
       filename_trial_times="${timings_OUT_DIR}/${k}-${l}-${f}-${failure_trials}"
       if [ -f "$filename_trial_times" ]; then
         rm $filename_trial_times
@@ -42,7 +42,7 @@ for l in $factors; do #load factors
     done
   done
 done 
-COMMENT
+#COMMENT
 
 factors='2.0'
 failure='0'
@@ -52,7 +52,7 @@ for l in $factors; do #load factors
   for f in $failure; do #failure rates
     filename_config_times="${timings_OUT_DIR}/${l}-${f}-${failure_trials}"
     touch $filename_config_times
-    for k in {50000000..300000000..50000000}; do #num key-val pairs
+    for k in {50000000..900000000..50000000}; do #num key-val pairs
       filename_trial_times="${timings_OUT_DIR}/${k}-${l}-${f}-${failure_trials}"
       if [ -f "$filename_trial_times" ]; then
         rm $filename_trial_times
@@ -77,9 +77,9 @@ for l in $factors; do #load factors
 done 
 COMMENT
 
-sizes='500000000'
+sizes='1450000000'
 factors='1.03'
-#: <<'COMMENT'
+: <<'COMMENT'
 #run through all the failures for a size-factor configuration:
 for k in $sizes; do #num key-val pairs
   for l in $factors; do #load factor
@@ -108,10 +108,10 @@ for k in $sizes; do #num key-val pairs
     done
   done
 done 
-#COMMENT
+COMMENT
 
 
-sizes='300000000'
+sizes='900000000'
 factors='2.0'
 : <<'COMMENT'
 #run through all the failures for a size-factor configuration:
@@ -145,7 +145,7 @@ done
 COMMENT
 
 factors='1.03 1.10 1.15 1.25 1.40 1.50 1.60 1.75 1.90 2.0'
-sizes='300000000'
+sizes='900000000'
 failure='0'
 : <<'COMMENT'
 #run through all the factors for a size-failure configuration:
@@ -153,7 +153,6 @@ for k in $sizes; do #num key-val pairs
   for f in $failure; do #failure rates
     filename_config_times="${timings_OUT_DIR}/${k}-${f}-${failure_trials}"
     touch $filename_config_times
-    #for k in {25000000..500000000..25000000}; do #num key-val pairs
     for l in $factors; do #load factors
       filename_trial_times="${timings_OUT_DIR}/${k}-${l}-${f}-${failure_trials}"
       if [ -f "$filename_trial_times" ]; then
@@ -175,7 +174,7 @@ done
 COMMENT
 
 factors='1.03 1.10 1.15 1.25 1.40 1.50 1.60 1.75 1.90 2.0'
-sizes='300000000'
+sizes='900000000'
 failure='5'
 : <<'COMMENT'
 #run through all the factors for a size-failure configuration:
@@ -183,7 +182,6 @@ for k in $sizes; do #num key-val pairs
   for f in $failure; do #failure rates
     filename_config_times="${timings_OUT_DIR}/${k}-${f}-${failure_trials}"
     touch $filename_config_times
-    #for k in {25000000..500000000..25000000}; do #num key-val pairs
     for l in $factors; do #load factors
       filename_trial_times="${timings_OUT_DIR}/${k}-${l}-${f}-${failure_trials}"
       if [ -f "$filename_trial_times" ]; then
