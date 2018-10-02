acb1bf8ec3fdada3c3fd3b6617e6c97683788e03
Brent Lessley <blessley@lbl.gov>
2018-10-01 17:40:12 -0700

    Added new directories for generating plots

---
+++ hashing-plots/generate-plot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+filename=$1
+minX=50000000
+maxX=500000000
+minY=0
+maxY=2000
+
+#Plot 1
+title_1='K40 GPU, 1.03 load factor'
+y_axis_1='Insertion Throughput (millions pairs/sec)'
+x_axis_1='# Key-Value Pairs (millions)'
+
+#Plot 2
+title_2='K40 GPU, 1.03 load factor'
+y_axis_2='Query Throughput (millions pairs/sec)'
+x_axis_2='# Key-Value Pairs (millions)'
+
+python 
+
+
