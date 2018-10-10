da96e1f0b139f5f1e595b4eb049c602555d18b0e
Samuel Li <shaomenglee@gmail.com>
2018-10-10 11:58:34 -0600

    new results out

---
+++ results/Oct09/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH -J CPU-Oct09
+#SBATCH -n 72
+#SBATCH --ntasks-per-node=72
+#SBATCH --mem=370G
+#SBATCH -t 24:00:00
+#SBATCH -A nvst0001
+#SBATCH -p dav
+#SBATCH -C skylake
+
+export TMPDIR=/glade/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+module remove intel openmpi
+module load gnu/6.4.0 cmake 
+
+/glade/work/shaomeng/XeonGold/hashfight/results/Oct09/run-all-cpu-Oct09.sh
