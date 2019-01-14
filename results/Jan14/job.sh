5bea4d7910012a22045ff8e05308c995f2ad5969
Samuel Li <shaomenglee@gmail.com>
2019-01-14 10:32:39 -0700

    add job output

---
+++ results/Jan14/job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#SBATCH -J CPU-Jan14
+#SBATCH -n 1
+#SBATCH -c 72
+#SBATCH --mem=370G
+#SBATCH -t 24:00:00
+#SBATCH -A nvst0001
+#SBATCH -p dav
+#SBATCH -C skylake
+
+export TMPDIR=/glade/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+module remove intel openmpi
+module load gnu/6.4.0 cmake 
+
+/glade/work/shaomeng/XeonGold/hashfight/results/Jan14/run-all-cpu-Jan14.sh
+
